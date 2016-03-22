14cc4fb0618133453fa95e6c4631d69bed1c10b1
dilanka92 <dilz_rulz@live.com>
2016-03-22 09:32:05 +0530

    init msg

---
+++ app/src/main/java/com/sourcey/materiallogindemo/MessageFactory.java
@@ -0,0 +1,22 @@
+package com.sourcey.materiallogindemo;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class MessageFactory {
+
+    public void CreateMsg(String title, String msg, Context context) {
+        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(context);
+        dlgAlert.setTitle(title);
+        dlgAlert.setMessage(msg);
+        dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // finish();
+                dialog.dismiss();
+            }
+        });
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
+}
