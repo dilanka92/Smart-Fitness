276f4eacecc6dd01806b230a256b5f50e5edc03a
dilanka92 <dilz_rulz@live.com>
2016-03-22 09:20:27 +0530

    Initial commit

---
+++ app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
@@ -0,0 +1,173 @@
+package com.sourcey.materiallogindemo;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    @Bind(R.id.input_name)
+    EditText _nameText;
+    @Bind(R.id.input_email)
+    EditText _emailText;
+    @Bind(R.id.input_age)
+    EditText _ageText;
+    @Bind(R.id.input_height)
+    EditText _heightText;
+    @Bind(R.id.input_weight)
+    EditText _weightText;
+    @Bind(R.id.input_password)
+    EditText _passwordText;
+    @Bind(R.id.btn_signup)
+    Button _signupButton;
+    @Bind(R.id.link_login)
+    TextView _loginLink;
+
+    // TODO: Implement Spinner.
+//    @Bind(R.id.input_lifestyle) Spinner _input_lifestyleText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String age = _ageText.getText().toString();
+        String height = _heightText.getText().toString();
+        String weight = _weightText.getText().toString();
+
+//        String lifestyle = _input_lifestyleText.getSelectedItem().toString();
+
+        // TODO: Implement signup logic.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Registration failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String age = _ageText.getText().toString();
+        String height = _heightText.getText().toString();
+        String weight = _weightText.getText().toString();
+
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+        if (age.isEmpty()) {
+            _ageText.setError("enter age");
+            valid = false;
+        } else {
+            _ageText.setError(null);
+        }
+        if (height.isEmpty()) {
+            _heightText.setError("enter height");
+            valid = false;
+        } else {
+            _heightText.setError(null);
+        }
+        if (weight.isEmpty()) {
+            _weightText.setError("enter weight");
+            valid = false;
+        } else {
+            _weightText.setError(null);
+        }
+//        if (lifestyle.isEmpty()) {
+//            ((TextView)_input_lifestyleText.getSelectedView()).setError("select lifestyle");
+//            valid = false;
+//        } else {
+//           // _input_lifestyleText.setError(null);
+//            ((TextView)_input_lifestyleText.getSelectedView()).setError(null);
+//        }
+
+        return valid;
+    }
+}
